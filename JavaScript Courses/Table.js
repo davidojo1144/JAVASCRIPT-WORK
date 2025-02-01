06dfcb57a9c599c17832bfe432cb724d5a2a2769
davidojo1144 <davco1144@gmail.com>
2025-02-01 14:58:01 +0100

    Decided to work on javascript

---
+++ JavaScript Courses/Table.js
@@ -0,0 +1,18 @@
+let user = [
+    {name: 'John Doe', age: 30, email: 'john.doe@example.com'},
+    {name: 'Jane Smith', age: 25, email: 'jane.smith@example.com'},
+    {name: 'Bob Johnson', age: 35, email: 'bob.johnson@example.com'}
+]
+
+let data = 10
+let money = 10
+
+let result = data + money
+
+console.log(result)
+console.table(user)
+
+let age = 20
+
+let msg = (age >= 18) ? "You are an adult" : "You are a minor"
+console.log(msg);
