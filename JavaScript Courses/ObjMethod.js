8452fde3a16607dfa0857d6bf71764eb27211f3d
davidojo1144 <davco1144@gmail.com>
2025-02-15 21:51:31 +0100

    Some JavaScripts

--- JavaScript Courses/ObjMethod.js
+++ JavaScript Courses/ObjMethod.js
@@ -3,10 +3,21 @@ const person = {
     firstName: "David",
     lastName: "Ojo",
     age:12,
-    regard : function greet(){
+    regard : function(){
         console.log("Hello There");
         
     }
 }
 
 person.regard()
+
+
+const person2 = { 
+    firstName2: "Daniel",
+    lastName2: "Ayo",
+    getFullName : function(){
+        return this.firstName2 + " " + this.lastName2
+    }
+}
+
+console.log(person2.getFullName())
