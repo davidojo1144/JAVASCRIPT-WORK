94018752c4e41b4c5d8c04aacf213510ead1b1ad
davidojo1144 <davco1144@gmail.com>
2025-02-17 18:26:35 +0100

    JavaScripts

--- JavaScript Courses/SetAndGet.js
+++ JavaScript Courses/SetAndGet.js
@@ -1,2 +1,15 @@
 // Setters and getters
-class Person
+class Person{
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+
+    get personName(){
+        return this.name
+    }
+
+    set personName(newName){
+        this.name = newName
+    }
+}
