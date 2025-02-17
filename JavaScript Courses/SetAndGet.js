24b94ac05b8bec3f70283ba642ca4ef3d4e2e402
davidojo1144 <davco1144@gmail.com>
2025-02-17 18:43:31 +0100

    JavaScripts

--- JavaScript Courses/SetAndGet.js
+++ JavaScript Courses/SetAndGet.js
@@ -22,15 +22,19 @@ class Person{
     }
 }
 
-// const person1 = new Person("David Ojo", 23)
-// console.log(person1.personName);
+const person1 = new Person("David Ojo", 23)
+console.log(person1.personName);
 
-// person1.personName = "Samuel Ojo"
-// console.log(person1.personName);
+person1.personName = "Samuel Ojo"
+console.log(person1.personName);
 
-// console.log(person1.age)
+console.log(person1.age)
 
 const person2 = new Person("Aliko Dangote", 50)
+console.log(person2)
+
+person2.personName = "Elon Musk"
+person2.personAge = 60
 console.log(person2)
 
 
