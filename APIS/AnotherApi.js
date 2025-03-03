dd5b943ed28549086de6a1b54ebf57d9bbfb111a
davidojo1144 <davco1144@gmail.com>
2025-03-03 09:58:06 +0100

    working with apis

---
+++ APIS/AnotherApi.js
@@ -0,0 +1,30 @@
+const api = 'https://jsonplaceholder.typicode.com/todos/'
+
+//fetching data using async await
+const fetchingData = async () => {
+    try {
+        const response = await fetch(api)
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log("Error: ", error)
+    }
+   
+}
+
+fetchingData()
+
+
+
+//fetching data using .then
+const myFetchData = () => {
+    try {
+        fetch(api)
+        .then(res => res.json())
+        .then(myData => console.log(myData))
+    } catch (error) {
+        console.log("Error: ", error)
+    }
+}
+
+myFetchData()
