a05d449725ceef303e39811c9f49e08c77bb1533
davidojo1144 <davco1144@gmail.com>
2025-05-09 06:30:34 +0100

    Working with event listeners

--- Event listeners/script.js
+++ Event listeners/script.js
@@ -1,8 +1,12 @@
 const heading = document.querySelector(".heading"); 
-const userInput = document.querySelector(".inputfield"); 
+const userInput = document.querySelector(".inputfield");
+let count = 1 
 
 // userInput.addEventListener("input", (event) => {
 //     heading.innerText = "Hello " + event.target.value + ", how are you ?";
 // });
 
-userInput.addEventListener
+userInput.addEventListener("input", (event)=> {
+    //heading.innerText = event.target.value + " " + count ++
+    event.target.value === "" ? count = 1  : heading.innerText = event.target.value + " " + count ++
+})
