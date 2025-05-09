6b4b6ca7f4e7f480a1c90211845a8ff8a4d92324
davidojo1144 <davco1144@gmail.com>
2025-05-09 06:03:47 +0100

    Working with event listeners

--- Event listeners/script.js
+++ Event listeners/script.js
@@ -1,6 +1,8 @@
-const heading = document.querySelector("heading")
-const userInput = document.querySelector("inputfield")
+const heading = document.querySelector(".heading"); 
+const userInput = document.querySelector(".inputfield"); 
 
-userInput.addEventListener("input", (event)=> {
-    heading.innerText = "Hello " + event.target.value + " how are you"
-})
+// userInput.addEventListener("input", (event) => {
+//     heading.innerText = "Hello " + event.target.value + ", how are you ?";
+// });
+
+userInput.addEventListener
