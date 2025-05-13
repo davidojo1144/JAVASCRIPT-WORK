d3eaad6bf16a0d376b674e61e5e6b1b7b99b869a
davidojo1144 <davco1144@gmail.com>
2025-05-13 20:45:56 +0100

    Event listeners

--- Event listeners/js_eventlistener/key.js
+++ Event listeners/js_eventlistener/key.js
@@ -2,8 +2,8 @@
 log = console.log
 const init = ()=> {
     document.addEventListener("keydown", (event)=> {
-        if (event.key === "Enter") {
-            log("The Enter key was clicked")
+        if (event.key === "w" || event.key === "W") {
+            log("The W key was clicked", event.key)
         }
     })
 }
